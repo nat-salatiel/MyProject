f9fdd4df055447ca016a55aecb2dd395855747e4
Natalia Salatiel <nsalatimel91@gmail.com>
2024-07-12 11:47:13 -0300

    Aprendendo cálculos aritméticos com Python

---
+++ MyCalcs.py
@@ -0,0 +1,16 @@
+#Demonstração do uso de operadores!
+
+x = int(input("Digite um número: "))
+y = int(input("Digite outro número: "))
+
+soma = x + y
+print("O resultado da soma é: ", soma)
+
+subtracao = x - y
+print("O resultado da subtração é: ", subtracao)
+
+multiplicacao = x * y
+print("O resultado da multiplicação é: ", multiplicacao)
+
+divisao = x / y
+print("O resultado da divisão é: ", divisao)
---
+++ MyOperators.py
@@ -0,0 +1,16 @@
+#Demonstração de expressões complexas
+
+#Declaração de varáveis que iremos usar
+
+x = 10; y = 20; z = 30
+valor = x * (y / 10 + z / 10) - 25
+print("O resultado da expressão #1 é: ", valor)
+
+valor = ((y - x) + (z - x)) / 3
+print("O resultado da expressão #2 é: ", valor)
+
+valor = (x + (y * 2 + (z / 10))) - 18
+print("O resultado da expressão #3 é: ", valor)
+
+#Antes de executar estes códigos, seria possível 
+#calcular "de cabeça" esse valores? Sim!
