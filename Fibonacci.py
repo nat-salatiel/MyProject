5d63d244cb62c8a385f62407fd69f2dbfeba5309
Natalia Salatiel <nsalatimel91@gmail.com>
2024-08-05 11:28:42 -0300

    Adicinando o break para melhorar o código

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,10 +3,12 @@ b = 1
 resultado = 0
 
 
-while resultado <= 1500:
+while resultado < 2000:
     resultado = a + b 
     a = b
     b = resultado
+    if resultado > 2000: 
+        break
     print(resultado)
     
    
